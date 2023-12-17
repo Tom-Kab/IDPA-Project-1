797a9dae003bd1c17632f25945fc290fa244905c
Tom-Kab <marc.aziz02@gmail.com>
2023-12-18 00:12:26 +0200

    Added clustering plots to frontend

--- searchAppFinal.py
+++ searchAppFinal.py
@@ -1,6 +1,11 @@
+import pandas as pd
 import streamlit as st
 from elasticsearch import Elasticsearch
 from sentence_transformers import SentenceTransformer
+from sklearn.cluster import KMeans, AgglomerativeClustering
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 indexName = "all_content"
 
@@ -18,6 +23,29 @@ else:
     print("Oops!! Can not connect to Elasticsearch!")
 
 
+def cluster(clustering_model):
+    model = SentenceTransformer('all-mpnet-base-v2')
+    df = pd.read_csv('output_data/stemmed_strings.csv')
+    corpus_embeddings = model.encode(df.stemmed_string)
+
+    if clustering_model == "KMeans":
+        kmeans = KMeans(n_clusters=5)
+        cluster_labels = kmeans.fit_predict(corpus_embeddings)
+        plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x')
+    elif clustering_model == "Agglomerative":
+        agglomerative = AgglomerativeClustering(n_clusters=5)
+        cluster_labels = agglomerative.fit_predict(corpus_embeddings)
+    else:
+        raise ValueError("Invalid clustering algorithm")
+
+    # Generate scatter plot
+    plt.scatter(corpus_embeddings[:, 0], corpus_embeddings[:, 1], c=cluster_labels)
+    plt.xlabel("Dimension 1")
+    plt.ylabel("Dimension 2")
+    plt.title("Clustering Results")
+    plt.savefig("output_data/plot_image.png")
+
+    return cluster_labels
 
 
 def search(input_keyword):
@@ -36,6 +64,15 @@ def search(input_keyword):
     return results
 
 def main():
+    st.title("Choose your clustering algorithm")
+    clustering_algorithm = st.selectbox("Clustering Algorithm", ["KMeans", "Agglomerative"])
+    if st.button("Cluster!"):
+        cluster_labels = cluster(clustering_algorithm)
+        st.image('output_data/plot_image.png')
+    
+    
+    
+    
     st.title("Search Articles")
 
     # Input: User enters search query
@@ -72,6 +109,7 @@ def main():
                         except Exception as e:
                             print(e)
                         st.divider()
+                        
 
 
                     
