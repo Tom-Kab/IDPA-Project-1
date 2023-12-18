5534de8131adcb7a0d075cb35b09d2e2268532c9
Tom-Kab <marc.aziz02@gmail.com>
2023-12-18 09:07:09 +0200

    Final Commit

--- searchAppFinal.py
+++ searchAppFinal.py
@@ -23,25 +23,23 @@ else:
     print("Oops!! Can not connect to Elasticsearch!")
 
 
-def cluster(clustering_model):
+def cluster(clustering_model, clusternumber, cluster_linkage="complete"):
     model = SentenceTransformer('all-mpnet-base-v2')
     df = pd.read_csv('output_data/stemmed_strings.csv')
     corpus_embeddings = model.encode(df.stemmed_string)
 
     if clustering_model == "KMeans":
-        kmeans = KMeans(n_clusters=5)
+        kmeans = KMeans(n_clusters=clusternumber, max_iter = 300, tol = 1e-4, algorithm = 'elkan')
         cluster_labels = kmeans.fit_predict(corpus_embeddings)
         plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x')
     elif clustering_model == "Agglomerative":
-        agglomerative = AgglomerativeClustering(n_clusters=5)
+        agglomerative = AgglomerativeClustering(n_clusters=clusternumber, linkage=cluster_linkage)
         cluster_labels = agglomerative.fit_predict(corpus_embeddings)
     else:
         raise ValueError("Invalid clustering algorithm")
 
     # Generate scatter plot
     plt.scatter(corpus_embeddings[:, 0], corpus_embeddings[:, 1], c=cluster_labels)
-    plt.xlabel("Dimension 1")
-    plt.ylabel("Dimension 2")
     plt.title("Clustering Results")
     plt.savefig("output_data/plot_image.png")
 
@@ -56,7 +54,7 @@ def search(input_keyword):
         "field": "ContentVector",
         "query_vector": vector_of_input_keyword,
         "k": 10,
-        "num_candidates": 10000
+        "num_candidates": 10000,                               
     }
     res = es.knn_search(index="all_content", knn=query, source=["title_article", "title", "content"])
     results = res["hits"]["hits"]
@@ -66,12 +64,15 @@ def search(input_keyword):
 def main():
     st.title("Choose your clustering algorithm")
     clustering_algorithm = st.selectbox("Clustering Algorithm", ["KMeans", "Agglomerative"])
+    clusternumber = st.selectbox("Number of clusters", [2, 3, 4, 5, 6, 7, 8, 9, 10])
+    if clustering_algorithm == "Agglomerative":
+        cluster_linkage = st.selectbox("Linkage", ["single", "complete", "average"])
     if st.button("Cluster!"):
-        cluster_labels = cluster(clustering_algorithm)
+        if clustering_algorithm == "Agglomerative":
+            cluster_labels = cluster(clustering_algorithm, clusternumber, cluster_linkage)
+        else:
+            cluster_labels = cluster(clustering_algorithm, clusternumber)
         st.image('output_data/plot_image.png')
-    
-    
-    
     
     st.title("Search Articles")
 
@@ -82,6 +83,11 @@ def main():
     if st.button("Search"):
         if search_query:
             # Perform the search and get results
+            # if clustering_algorithm == "Agglomerative":
+            #     cluster_labels = cluster(clustering_algorithm, clusternumber, cluster_linkage)
+            # else:
+            #     cluster_labels = cluster(clustering_algorithm, clusternumber)
+
             results = search(search_query)
 
             # Display search results
